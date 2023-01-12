2924c43d496453898bc6397326497fb4da972f41
kayo-b <kyo.bhz@gmail.com>
2023-01-12 21:42:16 +0100

    returning all possible moves, but not in a formated structure that can be worked on for the next phase, need to convert nested arrays to some more complex structure that can hold multiple conections

--- script.js
+++ script.js
@@ -23,17 +23,16 @@
 //Figuring out all the possible positions of [x,y]
 //if position [x,y] can be reached from current position adding or subtracting x +- 1 y +- 2 or x+2 y+1, 
 //then both are connected
-let grid =  [[0,1,2,3,4,5,6,7],
-            [0,1,2,3,4,5,6,7]]
+let grid =  [[0,1,2],
+            [0,1,2]]
 let gridCoordEx = [];
 let position = [];
 //Generate all grid coordinates
-for(let x = 0; x < 8; x++) {
-    for(let y = 0; y < 8;y++) {
+for(let x = 0; x < 3; x++) {
+    for(let y = 0; y < 3;y++) {
         gridCoordEx.push([grid[0][x],grid[1][y]])
     }
 }
-
 // position = [0,1]
 // position[1] = position[1]+1
 // console.log(position)
@@ -53,14 +52,15 @@ let X = [ 2, 1, -1, -2, -2, -1, 1, 2 ];
 let Y = [ 1, 2, 2, 1, -1, -2, -2, -1 ];
 let kmoves = []
 function recursiveKnight(array, gridCoord = gridCoordEx) {
-    if(array[0] > 7 || array[1] > 7 || array[0] < 0 || array[1] < 0) return 
+    if(array[0] > 2 || array[1] > 2 || array[0] < 0 || array[1] < 0) return 
     let temp = array;
         gridCoord.forEach(item => { 
             if(item[0] === temp[0] && item[1] === temp[1]) {
+                
                 let index = gridCoord.indexOf(item)
-                let newGrid = gridCoord;
+                let newGrid = gridCoord;            
                 newGrid.splice(index, 1)
-
+    
                 for(let i = 0; i < 8;i++) {
                     let k2 = recursiveKnight([temp[0] + X[i], temp[1] + Y[i]], newGrid)
                     if(k2 != undefined) temp.push(k2);
@@ -75,5 +75,19 @@ function recursiveKnight(array, gridCoord = gridCoordEx) {
 
 let x = recursiveKnight([0,0])
 // console.log(x)
+// console.log(x[2].length - 2)
+// console.log(x)
 console.log(kmoves.length)
+console.log(x[2])
 console.log(JSON.stringify(x))
+
+function printArray(moves) {
+    if(moves === undefined) return
+    console.log([moves[0],moves[1]] + " > " + moves[2] + " || " + moves[3]);
+    printArray(moves[2]);
+}
+printArray(x)
+
+//At this point, the function is able to return a nested array that contains all possible moves that a knight can make.
+//Next Step: unpack de nested arrays while keeping the relation between each position and convert it into a
+//data structure that represents all conections so that a shortest-path function can be created and applied.
--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,23 +1,2 @@
-let grid =  [[0,1,2,3,4,5,6,7],
-            [0,1,2,3,4,5,6,7]]
-let gridCoordEx = [];
-let position = [];
-// let count = 0
-
-// while(count < 37) {
-//     newArr.push([grid[0][x],grid[1][y]]);
-//     if(x < 6 && y === 6) {
-//         x += 2;
-//         y += 1;
-//     }
-// }
-
-
-
-for(let x = 0; x < 8; x++) {
-    for(let y = 0; y < 8;y++) {
-        gridCoordEx.push([grid[0][x],grid[1][y]])
-    }
-}
-
-console.log(gridCoordEx)
+console.log(x[2].length - 2)
+// console.log(x)
