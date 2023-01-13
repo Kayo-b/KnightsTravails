e85338a0527bded781048da416e6d2f2eeb5ed93
kayo-b <kyo.bhz@gmail.com>
2023-01-13 23:22:08 +0100

    created new function that takes the resulting possible position arrays and organized them so that they can be used to create a graph with correct edges and vortexes

--- script.js
+++ script.js
@@ -23,21 +23,16 @@
 //Figuring out all the possible positions of [x,y]
 //if position [x,y] can be reached from current position adding or subtracting x +- 1 y +- 2 or x+2 y+1, 
 //then both are connected
-let grid =  [[0,1,2],
-            [0,1,2]]
+let grid =  [[0,1,2,3,4,5,6,7],
+            [0,1,2,3,4,5,6,7]]
 let gridCoordEx = [];
 let position = [];
 //Generate all grid coordinates
-for(let x = 0; x < 3; x++) {
-    for(let y = 0; y < 3;y++) {
+for(let x = 0; x < 7; x++) {
+    for(let y = 0; y < 7;y++) {
         gridCoordEx.push([grid[0][x],grid[1][y]])
     }
 }
-// position = [0,1]
-// position[1] = position[1]+1
-// console.log(position)
-// gridCoord.forEach(item => console.log(item[0]===position[0]+1 && item[1] === position[1]))
-
 
 //logic steps:
 // 1. execute function with starting point [0,0] as parameter
@@ -51,43 +46,55 @@ for(let x = 0; x < 3; x++) {
 let X = [ 2, 1, -1, -2, -2, -1, 1, 2 ];
 let Y = [ 1, 2, 2, 1, -1, -2, -2, -1 ];
 let kmoves = []
+let temp = {}
 function recursiveKnight(array, gridCoord = gridCoordEx) {
-    if(array[0] > 2 || array[1] > 2 || array[0] < 0 || array[1] < 0) return 
+    if(array[0] > 7 || array[1] > 7 || array[0] < 0 || array[1] < 0) return 
     let temp = array;
+
         gridCoord.forEach(item => { 
-            if(item[0] === temp[0] && item[1] === temp[1]) {
+            if(item[0] === array[0] && item[1] === array[1]) {
                 
                 let index = gridCoord.indexOf(item)
                 let newGrid = gridCoord;            
-                newGrid.splice(index, 1)
+                newGrid.splice(index, 1);
     
                 for(let i = 0; i < 8;i++) {
-                    let k2 = recursiveKnight([temp[0] + X[i], temp[1] + Y[i]], newGrid)
-                    if(k2 != undefined) temp.push(k2);
+                    let k2 = recursiveKnight([array[0] + X[i], array[1] + Y[i]], newGrid, temp);
+                    if(k2 != undefined) temp.push(k2)
+                    
                     if(k2 != undefined) kmoves.push(k2);
                 }
-                }
-            })
-            return temp;
-         
+            }
+        })
+        return temp;
 }
-            
 
 let x = recursiveKnight([0,0])
 // console.log(x)
-// console.log(x[2].length - 2)
-// console.log(x)
-console.log(kmoves.length)
-console.log(x[2])
-console.log(JSON.stringify(x))
-
-function printArray(moves) {
-    if(moves === undefined) return
-    console.log([moves[0],moves[1]] + " > " + moves[2] + " || " + moves[3]);
-    printArray(moves[2]);
-}
-printArray(x)
 
 //At this point, the function is able to return a nested array that contains all possible moves that a knight can make.
 //Next Step: unpack de nested arrays while keeping the relation between each position and convert it into a
 //data structure that represents all conections so that a shortest-path function can be created and applied.
+
+//Function unpackArrays takes the resulting nested array from the previous function and returns a list
+//containing all nodes and its conections, being that the index 0 is the starting node(position in chessboard) and the other 
+//following indexes are the possible positions where the knight can move.
+let arrList = []
+function unpackArrays(arr) {
+    let newArr = [[arr[0],arr[1]]]
+    if(arr.length > 2) {
+        for(let x = 2; x < arr.length;x++) {
+            if(arr[x].length > 2) {
+                newArr.push([arr[x][0],arr[x][1]]) 
+                unpackArrays(arr[x]);
+                
+            } else newArr.push(arr[x])
+        }
+    }
+    arrList.push(newArr)
+    return newArr
+}
+
+unpackArrays(x)
+// console.log(kmoves)
+console.log(arrList)
--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,2 +1,12 @@
-console.log(x[2].length - 2)
-// console.log(x)
+let obj = {}
+let obj2 = {}
+let obj3 = {}
+obj.position = [0,0];
+obj2.position = [2,1];
+obj2.next = obj;
+obj3.position = [1,2];
+obj3.next = obj
+obj.next2 = obj3
+obj.next = obj2
+
+console.log(obj[0])
